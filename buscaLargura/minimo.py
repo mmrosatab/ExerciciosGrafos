c169ab5f906028da7430dae0688bed10827d737c
Mayara Marques <mmrosatab@hotmail.com>
2018-09-06 16:00:32 -0300

    Update minimo.py
    Atualização algoritmo caminho mínimo. Funcionando

--- buscaLargura/minimo.py
+++ buscaLargura/minimo.py
@@ -1,14 +1,59 @@
-lAdj = {}
+import sys
+
+G = {}
 listaAux = []
+dist = {}
+cores = {}
+fila = []
+pred = {}
 
 arquivo = open("entrada.txt","+r")
 
 linhas = arquivo.read().splitlines()
 
-#gerando lista de adjacencia
-for x in linhas:
-	listaAux = x.split(" ")
-	lAdj[listaAux[0]] = listaAux[1:] 
+
+def main():
+
+	#gerando lista de adjacencia
+	for x in linhas:
+		listaAux = x.split(" ")
+		G[listaAux[0]] = listaAux[1:] 
+		
+	for x in G:
+		if x != "A":
+			dist[x] = 0
+			cores[x] = "branca"
+
+	dist["A"] = 0
+	cores["A"] = "azul"
+	pred["A"] = None
+	fila.append("A")
+
+	#print("Grafo")
+	#print(G)
+	#print("Distancia")
+	#print(dist)
+	#print("Cores")
+	#print(cores)
+	#print(fila)
+
+	while len(fila) > 0:
+		u = fila[0]
+		for v in G[u]:
+			if cores[v] == "branca": # vertice nunca foi visitado
+				cores[v] = "azul"
+				dist[v] = dist[u] + 1
+				pred[v] = u # seta predecessor de um vertice
+				fila.append(v)
+		h = input("Para")
+		
+		#desenfila
+		fila.pop(0)
+
+		cores[u] = "azul"
 
 
-print(lAdj)
+	print(dist)
+	
+if __name__ == '__main__':
+	main()
