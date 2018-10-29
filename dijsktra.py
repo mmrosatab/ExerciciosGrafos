64c9977e135d533285f72b6d7c6dcfc1402679f3
Mayara Marques <mmrosatab@hotmail.com>
2018-10-29 18:31:45 -0300

    Update dijsktra.py

--- dijsktra.py
+++ dijsktra.py
@@ -7,7 +7,6 @@ class Graph:
 
         self.data = {}
         self.dist = {}
-        self.d = {}
         self.pred = {}
 
     def read(self,filename=None,ignore=0):
@@ -22,9 +21,7 @@ class Graph:
             ignore -= 1
         
         aux = f.read().splitlines()
-
-        s = aux[0]
-              
+  
         data = {}
 
         for line in aux[1:]:
@@ -36,54 +33,88 @@ class Graph:
             #print(neighbours)
 
             if data.get(u) == None:
+
                 l = {}
-                l[neighbours[0]] = neighbours[1]
+                l[neighbours[0]] = int(neighbours[1])
                 data[u] = l
             else:
-                data[u][neighbours[0]] = neighbours[1]  
+                data[u][neighbours[0]] = int(neighbours[1])  
+
+            if data.get(neighbours[0]) == None: # caso um dos vizinhos nao exista aresta incidindo em outro vertice
+                #print("Entrei")
+                #print(neighbours[0])
+                data[neighbours[0]] = {} 
 
         self.data = data    
         #print(data)
-        return s
+        return aux[0]
     
-'''
-def getTupleValue(G,u,v):
 
-    for x in G.data[u]:
-        if x[0] == v:
-            return x[1]
-    
+def pesoAresta(G,u,v):
+
+    for x,y in G.data[u].iteritems():
+        if x == v:
+            return y
+   
 
 def relaxa(G,u,v):
     
-    w = getTupleValue(G,u,v)
 
-    if G.d[u] + w < G.d[v]:
-       G.d[v] = G.d[u] + w
+    w = pesoAresta(G,u,v)
+    
+    #print(G.dist[u])
+    #print(G.dist[v])
+
+    if G.dist[u] + w < G.dist[v]:
+       G.dist[v] = G.dist[u] + w
        G.pred[v] = u
-'''
+
 
 def inicializa(G,s):
 
     for x in G.data:
-        print(x)
-        G.dist[x] = float("inf")
+        
+        #print("data:",x)
+
+        G.dist[x] = 1000
         G.pred[x] = -1
-    G.d[s] = 0    
+    
+    G.dist[s] = 0    
+
 
 def caminhoMinimo(G,s):
 
-    pass
+    G.dist = dict(sorted(G.dist.items(), key=lambda t:t[1]))
+    #print("Estado Distancias: ", G.dist)
 
-    
+    while len(G.dist) > 0:
+
+        # minimo da fila
+        u = min(G.dist, key=G.dist.get)
+        
+
+        for k,v in G.data[u].iteritems():
+
+            relaxa(G,u,k)
+
+        del G.dist[u]
+
+        G.dist = dict(sorted(G.dist.items(), key=lambda t:t[1]))
+        #print("Estado Distancias: ", G.dist)
+
+
 G = Graph()
 s = G.read("edj.txt",ignore=0)
+
+print(s)
 print(G.data)
-print(G.dist)
-print(G.pred)
 
 inicializa(G,s)
 
-print(G.data)
-print(G.dist)
-print(G.pred)
+print("Distancias:", G.dist)
+print("Predecessores: ", G.pred)
+
+caminhoMinimo(G,s)
+
+#print("Distancias:", G.dist)
+#print("Predecessores: ", G.pred)
